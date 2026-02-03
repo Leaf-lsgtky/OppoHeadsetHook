2a2f89f58c61ecfb0b68a44c9bf225d28c9adbd5
“Leaf-lsgtky“ <1587968048@qq.com>
2026-02-03 18:54:02 +0800

    fix3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "moe.chenxy.oppoheadset"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "moe.chenxy.oppoheadset"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
